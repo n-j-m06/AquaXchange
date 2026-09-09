72e498d354eaeee98b5cc3e9aace1cb882a227c2
n-j-m06 <menonniranjanj@gmail.com>
2026-09-10 01:48:59 +0530

    Fix frontend repository tracking

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,7 +25,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://aquaxchange-backend.onrender.com",
+        "https://aqua-xchange-git-main-niranjan3.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
